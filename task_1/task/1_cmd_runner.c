8f637797b0c1ddfa362a97c26b73e2c2d4812593
EllyTale <alenka.kazmina@gmail.com>
2017-09-29 23:48:49 +0300

    task_1;variant_1

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -9,3 +9,26 @@
    a) if execvp is used, string splitting into "path" and "args" is all you need.
 3. Collect exit codes via waitpid/WEXITSTATUS.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int spawn(char* program, char** arg_list)
+{
+ pid_t child;
+ child = fork();//создание копии текущего процесса
+ if(child!=0) return child;//это родитель
+ else execvp(program, arg_list);
+ fprintf(stderr,"error");
+abort();
+}
+
+int main ()
+{
+ char* arg_list[] = {"ls", "-l", "/", NULL};
+ spawn ("ls", arg_list);
+ printf ("done\n");
+ return 0;
+}
+
