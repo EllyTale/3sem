18d1d902b84a41e6f33555c85f929f51b94830e4
EllyTale <alenka.kazmina@gmail.com>
2017-12-16 01:45:40 +0300

    added

---
+++ task_1.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+int main() 
+{    
+    pid_t pid;    
+    int status;    
+    char path[1000];    
+    char *arg[100];     
+    char line[1000];   
+    char arrays[100][100]={""} ; 
+    while(1) 
+    {        
+        int i = 0, j = 0, k=0, p=0;   
+        fgets(line, 1000, stdin);  
+        int lenght = strlen(line); 
+        if (lenght == 1)        
+        {           
+            return -1;       
+        }        
+        for (i = 0; i < lenght; i++)
+        {           
+            if (line[i] == ' '|| line[i] == '\0' || line[i] == '\n')      
+            {               
+                arrays[j][i-p] = '\0';     
+                p=i+1;           
+                arg[j]=arrays[j];   
+                j++;            
+                if(j>=99)  break ;
+            }            
+            else            
+            {              
+                arrays[j][i-p] = line[i];  
+            }       
+            
+        }
+        arg[j]=NULL; 
+        switch(pid = fork()) 
+        {
+            case -1:
+                printf("error\n");
+            case 0:
+                execvp(arrays[0], arg);   
+            default:
+                wait(&status); 
+                printf("Ret code: %d\n", WEXITSTATUS(status));
+        } 
+    }
+    return 0;
+    
+}
